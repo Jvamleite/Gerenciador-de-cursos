da43164a29eb532cb268ca81cc957b0e4f7ef047
João Victor Amaral <joao.amaral@squadra.com.br>
2025-09-30 11:33:37 -0300

    Criando Dtos para o caso de uso de criação de curso

---
+++ src/GerenciadorDeCursos.Application/Dtos/Course/Request/CreateCourseRequestDto.cs
@@ -0,0 +1,4 @@
+﻿namespace GerenciadorDeCursos.Application.Dtos.Course.Request
+{
+    public record CreateCourseRequestDto(string Title, string TeachersName, DateTime InitialDate, DateTime FinalDate, int Vacancies, string Department);
+}
---
+++ src/GerenciadorDeCursos.Application/Dtos/Course/Response/CourseResponseDto.cs
@@ -0,0 +1,6 @@
+﻿using GerenciadorDeCursos.Domain.Entities.Course.Enums;
+
+namespace GerenciadorDeCursos.Application.Dtos.Course.Response
+{
+    public record CourseResponseDto(string Code, string Title, string Teacher, DateTime InitialDate, DateTime FinalDate, Status Status, int Vacancies);
+}
