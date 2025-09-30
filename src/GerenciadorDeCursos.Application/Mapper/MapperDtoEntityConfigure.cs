74b1909bc0f851954aec377b20f4a7ed345d9fac
João Victor Amaral <joao.amaral@squadra.com.br>
2025-09-30 11:36:36 -0300

    Criado mapper entre Dto e Entity de curso

---
+++ src/GerenciadorDeCursos.Application/Mapper/MapperDtoEntityConfigure.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using GerenciadorDeCursos.Application.Dtos.Course.Request;
+using GerenciadorDeCursos.Domain.Entities.Course;
+
+namespace GerenciadorDeCursos.Domain.Model.Mapper
+{
+    internal class MapperDtoEntityConfigure : Profile
+    {
+        public MapperDtoEntityConfigure()
+        {
+            CreateMap<CreateCourseRequestDto, CourseEntity>
+                ();
+        }
+    }
+}
