939543afbe979220632a6df87e72884ac15b5e37
João Victor Amaral <joao.amaral@squadra.com.br>
2025-09-30 08:35:52 -0300

    Reestruturando projetos e criando repository base

---
+++ src/GerenciadorDeCursos.Domain.Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace GerenciadorDeCursos.Domain.Repositories
+{
+    internal interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(int id);
+
+        Task AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(int id);
+    }
+}
