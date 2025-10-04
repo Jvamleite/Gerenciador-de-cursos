792088a92eea0b2bc7056f6432238f13e696dc20
João Victor Amaral <joao.amaral@squadra.com.br>
2025-10-03 21:09:46 -0300

    Correção erro swagger

--- src/GerenciadorDeCursos.API/Configurations/SwaggerConfig.cs
+++ src/GerenciadorDeCursos.API/Configurations/SwaggerConfig.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.OpenApi.Models;
-using System.Reflection;
 
 namespace GerenciadorDeCursos.API.Configurations
 {
@@ -10,9 +9,6 @@ namespace GerenciadorDeCursos.API.Configurations
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GerenciadorDeCursos.API", Version = "v1", Description = "Simulação de um portal de inscrição em cursos e cadastramento de usuários" });
-
-                string xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
             });
         }
     }
